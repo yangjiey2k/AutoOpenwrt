cb1fc624590c287a7aa107a0fbee7c603df1f228
YG <54274847+qqsir-dev@users.noreply.github.com>
2025-01-11 10:03:49 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -58,7 +58,7 @@ if [ -d *"luci-theme-argon"* ]; then
 	cd ./luci-theme-argon/
 
 	sed -i '/font-weight:/ {/normal\|!important/! s/\(font-weight:\s*\)[^;]*;/\1normal;/}' $(find ./luci-theme-argon -type f -iname "*.css")
-	sed -i "s/#5e72e4/#31a1a1/; s/#483d8b/#31a1a1/; s/'0.2'/'0.5'/; s/'none'/'bing'/" ./luci-app-argon-config/root/etc/config/argon
+	sed -i "s/#5e72e4/#31a1a1/; s/#5e72e4/#947f71/; s/'0.2'/'0.5'/; s/'none'/'bing'/" ./luci-app-argon-config/root/etc/config/argon
 
 	cd $PKG_PATH && echo "theme-argon has been fixed!"
 fi
