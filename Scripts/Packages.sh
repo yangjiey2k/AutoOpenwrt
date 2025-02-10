10e3f0d96f6531b3e6509de616b4e34341a8dd4b
YG <54274847+qqsir-dev@users.noreply.github.com>
2025-02-10 16:40:46 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
 #安装和更新软件包
-#UPDATE_PACKAGE() {
-#	local PKG_NAME=$1
-#	local PKG_REPO=$2
-#	local PKG_BRANCH=$3
-#	local PKG_SPECIAL=$4
-#	local REPO_NAME=$(echo $PKG_REPO | cut -d '/' -f 2)
-#
-#	find ./ ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -exec rm -rf {} +
-#
-#	git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
-#
-#	if [[ $PKG_SPECIAL == "pkg" ]]; then
-#		find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune -exec cp -rf {} ./ \;
-#		rm -rf ./$REPO_NAME/
-#	elif [[ $PKG_SPECIAL == "name" ]]; then
-#		mv -f $REPO_NAME $PKG_NAME
-#	fi
-#}
 UPDATE_PACKAGE() {
     local PKG_NAME=$1
     local PKG_REPO=$2
