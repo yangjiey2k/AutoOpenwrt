e01640dd78b5b9ec2cb4fb4e4ce3131fffb2b65b
YG <54274847+qqsir-dev@users.noreply.github.com>
2025-04-19 23:18:56 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -59,7 +59,7 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-m
 # Add restart dropbear firewall and DDNS
 SET_RESTART="./package/base-files/files/etc/uci-defaults/999_auto-restart.sh"
 sed -i "/exit/i/etc/init.d/firewall restart\n/etc/init.d/ddns restart\n" $SET_RESTART
-
+cat "$SET_RESTART"
 CFG_FILE="./package/base-files/files/bin/config_generate"
 #修改默认IP地址
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
